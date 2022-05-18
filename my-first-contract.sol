e52b47eace36f08dd053b1b3b2ebea4e7618ccce
Emile Calixte <calixte.emile@gmail.com>
2022-05-18 10:34:09 +0200

    First commit

---
+++ my-first-contract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17; // Specifies the version of Solidity that our code is written with
+
+contract Inbox { // Defines a new contract, that will have some number of methods and variables
+
+    // Declares all of the instance variables (and their types) that will exist in this contract
+
+    string public message;
+
+    // Defines different functions that will be members of this contract
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
